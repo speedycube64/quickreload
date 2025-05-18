5e8855fdff7badc405e90f6396fd3c6b6dacc6f3
speedycube64 <rubikscube641@gmail.com>
2025-05-17 22:13:38 -0400

    Made it work with Custom Map Resetter
    And now it works if you have it installed or don't have it installed :)

--- 1.8.9/src/main/java/net/fabricmc/quickreload/mixin/QuickReloadMixin.java
+++ 1.8.9/src/main/java/net/fabricmc/quickreload/mixin/QuickReloadMixin.java
@@ -1,43 +1,16 @@
 package net.fabricmc.quickreload.mixin;
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-import net.minecraft.client.MinecraftClient;
-=======
 import net.fabricmc.loader.api.FabricLoader;
->>>>>>> Stashed changes
-=======
-import net.fabricmc.loader.api.FabricLoader;
->>>>>>> Stashed changes
-=======
-import net.fabricmc.loader.api.FabricLoader;
->>>>>>> Stashed changes
 import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.world.ClientWorld;
-import net.minecraft.world.SaveHandler;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-import java.util.List;
-=======
 import net.fabricmc.quickreload.CMRHelper;
->>>>>>> Stashed changes
-=======
-import net.fabricmc.quickreload.CMRHelper;
->>>>>>> Stashed changes
-=======
-import net.fabricmc.quickreload.CMRHelper;
->>>>>>> Stashed changes
 
 @Mixin(GameMenuScreen.class)
 public class QuickReloadMixin extends Screen{
@@ -60,10 +33,6 @@ public class QuickReloadMixin extends Screen{
             // get world name
             String saveName = client.getServer().getLevelName();
 
-<<<<<<< Updated upstream
-            // reconnect
-            this.client.startIntegratedServer(saveName, saveName, null);
-=======
             // disconnect
             this.client.world.disconnect();
             this.client.connect((ClientWorld)null);
@@ -91,29 +60,7 @@ public class QuickReloadMixin extends Screen{
             else {
                 this.client.startIntegratedServer(saveName, saveName, null);
             }
->>>>>>> Stashed changes
         }
     }
 
 }
-
-//used for debugging to find the pause menu screen to mix into
-
-//@Mixin(MinecraftClient.class)
-//public class QuickReloadMixin {
-//
-//    @Shadow public Screen currentScreen;
-//
-//    @Inject(method = "setScreen", at = @At("HEAD"))
-//    private void onSetScreen(CallbackInfo ci) {
-//
-//        Screen screen = (Screen)(Object)(this.currentScreen);
-//
-//        if (screen != null) {
-//            System.out.println("Screen class: " + screen.getClass().getName());
-//        }
-//    }
-//}
-
-
-
